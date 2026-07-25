701bc2a6ded4d63cbc1e1d5e1d635d88eb0f8d19
Sai Supraja <saisupraja.annam48@gmail.com>
2026-07-25 08:40:55 +0530

    Fic API

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,9 @@
 import DynamicPage from "./[slug]/page";
 
-export default async function Home(props) {
-  return <DynamicPage params={Promise.resolve({ slug: "home" })} />;
+export default async function Home() {
+  return (
+    <DynamicPage
+      params={Promise.resolve({ slug: "home" })}
+    />
+  );
 }
