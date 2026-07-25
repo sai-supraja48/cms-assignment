977055b14da1e3834592afd76da5cac2bb6052d4
Sai Supraja <saisupraja.annam48@gmail.com>
2026-07-25 08:02:42 +0530

    Updated frontend API

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,22 +1,5 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-
-      <section className="flex flex-col items-center justify-center h-[80vh] bg-gray-100">
-        <h1 className="text-6xl font-bold">
-          Welcome to CMS
-        </h1>
-
-        <p className="mt-6 text-xl text-gray-600">
-          Dynamic Website powered by Next.js + Express + MongoDB
-        </p>
-      </section>
-
-      <Footer />
-    </>
-  );
+  redirect("/login");
 }
